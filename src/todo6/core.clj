f1bc533bab6aec21767da6a1dce298d1e0bb6073
rkulla <rkulla@albatross.(none)>
2012-12-20 23:29:09 -0800

    Added command prompt

--- src/todo6/core.clj
+++ src/todo6/core.clj
@@ -4,14 +4,29 @@
 
 (def todo-file (str (System/getenv "HOME") "/todo6.txt"))
 (def max-tasks 6)
-(def abort-msg-format 
+(def abort-message
   (format "Too many tasks. Trim down %s to %d lines\n" todo-file max-tasks))
+(def prompt "> ")
 
 (defn- validate-todo-contents [tasks]
   "Validates the contents of the todo file"
   (when (> (count tasks) max-tasks) 
-    (println abort-msg-format)
+    (println abort-message)
     (System/exit 0)))
+
+(defn prompt-user-input []
+  "Show a prompt and user input"
+  (print prompt)
+  (flush)
+  (read-line))
+
+(defn get-commands [tasks]
+  "Repeatedly get user commands"
+  (loop [input  
+    (prompt-user-input)]
+      (when (= input "ls")
+        (pprint/pprint tasks))
+        (recur (prompt-user-input))))
 
 (defn- parse-todo-file []
   "Parses out the tasks from the todo file"
@@ -20,10 +35,10 @@
       (try
         (slurp todo-file)
         (catch Exception e
-          (printf "Error: ") (.getMessage e))))]
+          (print "Error: ") (.getMessage e))))]
             (into (sorted-map) (zipmap (range 1 (inc (count lines))) lines))))
   (validate-todo-contents tasks)
-  (pprint/pprint tasks))
+  (get-commands tasks))
 
 (defn -main [& args]
   (parse-todo-file))
