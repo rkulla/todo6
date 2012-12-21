50805223ccb2ece06aa045be3ad259203f82000b
rkulla <rkulla@albatross.(none)>
2012-12-20 17:29:47 -0800

    code cleanup

--- src/todo6/core.clj
+++ src/todo6/core.clj
@@ -3,18 +3,21 @@
             [clojure.pprint :as pprint]))
 
 (def todo-file (str (System/getenv "HOME") "/todo6.txt"))
-
 (def max-tasks 6)
+(def abort-msg-format 
+  (format "Too many tasks. Trim down %s to %d lines\n" todo-file max-tasks))
+
+(defn- validate-todo-contents [tasks]
+  (when (> (count tasks) max-tasks) 
+    (println abort-msg-format)
+    (System/exit 0)))
 
 (defn- parse-todo-file []
-  (def todo 
-    (let [tasks (string/split-lines (slurp todo-file))] 
-      (into (sorted-map) (zipmap (range 1 (inc (count tasks))) tasks))))
-  (when (> (count todo) max-tasks) 
-    (println "Too many tasks." "Trim down" todo-file "to" max-tasks "\n")
-    (System/exit 0))
-  (pprint/pprint todo))
-
+  (def tasks 
+    (let [lines (string/split-lines (slurp todo-file))] 
+      (into (sorted-map) (zipmap (range 1 (inc (count lines))) lines))))
+  (validate-todo-contents tasks)
+  (pprint/pprint tasks))
 
 (defn -main [& args]
   (parse-todo-file))
