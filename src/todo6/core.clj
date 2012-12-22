3e20377f91c718129b1a57d40ed053cab3f5bc40
rkulla <rkulla@albatross.(none)>
2012-12-22 11:00:29 -0800

    Made it so you can mark tasks as done

--- src/todo6/core.clj
+++ src/todo6/core.clj
@@ -14,7 +14,7 @@
 
 (defn- validate-todo-contents [tasks]
   "Validates the contents of the todo file"
-  (when (> (count tasks) max-tasks) 
+  (when (> (count @tasks) max-tasks) 
     (println abort-message)
     (exit)))
 
@@ -29,33 +29,37 @@
   (if (some (partial = s) args)
     true false))
 
+(defn- get-first-int [s]
+  (Integer. (first (re-seq #"\d" s))))
+
 (defn- get-commands [tasks]
   "Repeatedly get user commands"
   (println help-message) 
   (loop [input  
     (prompt-user-input)]
       (cond
-        (is-in input "ls" "list") (pprint/pprint tasks)
+        (is-in input "ls" "list") (pprint/pprint @tasks)
         (apply is-in input (map str (range 1 (inc max-tasks))))
-          (println (get tasks (Integer. input)))
+          (println (get @tasks (Integer. input)))
         (is-in input "exit" "quit") (exit)
         (is-in input "?" "help") (println help-message)
-        (.startsWith input "done") (println 
-          (update-in tasks [(Integer. (first (re-seq #"\d" input))) :status] "done"))
+        (.startsWith input "done") 
+          (swap! tasks assoc-in [(get-first-int input) :status] "done")
         :else (println "No such command." help-message))
         (recur (prompt-user-input))))
 
 (defn- parse-todo-file []
   "Parses out the tasks from the todo file"
   (def tasks 
-    (let [lines (string/split-lines
-      (try
-        (slurp todo-file)
-        (catch Exception e
-          (print "Error: ") (.getMessage e))))]
-            (into (sorted-map) (zipmap (range 1 (inc (count lines)))
-              (map-indexed 
-                (fn [idx itm] idx {:name itm, :status "pending"}) lines)))))
+    (atom
+        (let [lines (string/split-lines
+        (try
+            (slurp todo-file)
+            (catch Exception e
+            (print "Error: ") (.getMessage e))))]
+                (into (sorted-map) (zipmap (range 1 (inc (count lines)))
+                (map-indexed 
+                    (fn [idx itm] idx {:name itm, :status "pending"}) lines))))))
   (validate-todo-contents tasks)
   (get-commands tasks))
 
