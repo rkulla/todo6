2a8867398986942a21c5060f0874d90f9db3eff5
rkulla <rkulla@albatross.(none)>
2012-12-21 22:49:10 -0800

    Enhanced core data structure

--- src/todo6/core.clj
+++ src/todo6/core.clj
@@ -6,7 +6,7 @@
 (def max-tasks 6)
 (def abort-message
   (format "Too many tasks. Trim down %s to %d lines\n" todo-file max-tasks))
-(def help-message "Commands: ?/help, ls/list, quit/exit")
+(def help-message "Commands: ?/help, ls/list, quit/exit, 1-6, done 1-6")
 (def prompt "> ")
 
 (defn- exit []
@@ -40,6 +40,8 @@
           (println (get tasks (Integer. input)))
         (is-in input "exit" "quit") (exit)
         (is-in input "?" "help") (println help-message)
+        (.startsWith input "done") (println 
+          (get-in tasks [(Integer. (first (re-seq #"\d" input))) :status]))
         :else (println "No such command." help-message))
         (recur (prompt-user-input))))
 
@@ -51,7 +53,9 @@
         (slurp todo-file)
         (catch Exception e
           (print "Error: ") (.getMessage e))))]
-            (into (sorted-map) (zipmap (range 1 (inc (count lines))) lines))))
+            (into (sorted-map) (zipmap (range 1 (inc (count lines)))
+              (map-indexed 
+                (fn [idx itm] idx {:name itm, :status "pending"}) lines)))))
   (validate-todo-contents tasks)
   (get-commands tasks))
 
